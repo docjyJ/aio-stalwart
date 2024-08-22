8de62f689c0ea75cc024b24f9ab601863d431681
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-08-22 11:03:29 +0200

    Update test.sh
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -5,12 +5,24 @@ docker() {
   podman "$@"
 }
 
-#
-# Work in progress
-#
+STEP_NB=3
+STEP_I=0
+print_step() {
+  STEP_I=$((STEP_I+1))
+  echo ""
+  echo "=====[$STEP_I/$STEP_NB]====="
+  echo "$3"
+}
 
+
+print_step "Build Docker Image"
 IMAGE=$(docker build . | tee /dev/tty | tail -n 1)
 
-echo "Script generated :"
+print_step "Generate prepare env"
+NC_DOMAIN="example.com"
+CADDY="my_caddy_example_com"
+STALWART="my_stalwart_example_com"
+docker run --rm -v $V_CADDY:/data caddy caddy reverse-proxy -i -f https://mail.example.com -t http://$STALWART:10003
 
+print_step "Test 1"
 docker run --rm --env AUTO_CONFIG_TLS_CERT=OFF  "$IMAGE" cat /opt/stalwart-mail/etc/config.toml
