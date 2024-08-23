d5f4e562d1e05589a5b03483a589655043fdfc4e
Jean-Yves <7360784+docjyJ@users.noreply.github.com>
2024-08-23 14:20:07 +0200

    Update test.sh
    Signed-off-by: Jean-Yves <7360784+docjyJ@users.noreply.github.com>

--- test.sh
+++ test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# NOTE: This script use podman instead of docker !
-docker() {
-  podman "$@"
-}
-
-STEP_NB=3
+STEP_NB=5
 STEP_I=0
 print_step() {
   STEP_I=$((STEP_I+1))
@@ -15,14 +10,25 @@ print_step() {
 }
 
 
-print_step "Build Docker Image"
-IMAGE=$(docker build . | tee /dev/tty | tail -n 1)
-
-print_step "Generate prepare env"
 NC_DOMAIN="example.com"
 CADDY="my_caddy_example_com"
 STALWART="my_stalwart_example_com"
-docker run --rm -v $V_CADDY:/data caddy caddy reverse-proxy -i -f https://mail.example.com -t http://$STALWART:10003
+SSL_PATH="caddy/certificates/acme-v02.api.letsencrypt.org-directory/mail.$NC_DOMAIN"
+
+print_step "Build Docker Image"
+docker build -t "$STALWART" .
+echo "docker run --rm -v $STALWART:/opt/stalwart-mail -v $CADDY:/caddy:ro --env "NC_DOMAIN=$NC_DOMAIN" $STALWART stalwart-cli --help"
+
+
+print_step "Generate prepare env"
+docker run --rm -v $CADDY:/ssl --entrypoint /bin/mkdir $STALWART -p "/ssl/$SSL_PATH/"
+docker run --rm -v $CADDY:/ssl --entrypoint /bin/openssl $STALWART req -x509 -noenc -subj "/C=AA/ST=A/O=A/CN=mail.$NC_DOMAIN" -keyout "/ssl/$SSL_PATH/mail.$NC_DOMAIN.key" -out "/ssl/$SSL_PATH/mail.$NC_DOMAIN.crt"
 
 print_step "Test 1"
-docker run --rm --env AUTO_CONFIG_TLS_CERT=OFF  "$IMAGE" cat /opt/stalwart-mail/etc/config.toml
+docker run --rm -v $CADDY:/caddy:ro -v $STALWART:/opt/stalwart-mail --env "NC_DOMAIN=$NC_DOMAIN" $STALWART cat /opt/stalwart-mail/etc/config.toml
+
+print_step "Test 2"
+docker run --rm -v $CADDY:/caddy:ro -v $STALWART:/opt/stalwart-mail --env "NC_DOMAIN=$NC_DOMAIN" $STALWART cat /opt/stalwart-mail/etc/config.toml
+
+print_step "Remove vollume"
+docker volume rm $STALWART $CADDY
